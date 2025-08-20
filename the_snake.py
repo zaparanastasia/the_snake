19c3492b448dd1a8107990a8393a929a796338e2
Anastasia U <uddinanastasia@gmail.com>
2025-08-20 21:45:47 +0200

    текст

--- the_snake.py
+++ the_snake.py
@@ -158,9 +158,8 @@ def main():
 
         # --- caption разбит на две строки для PEP8 ---
         caption = (
-            'Змейка | ESC - выход | Скорость: {} | '
-            'Рекорд длины: {}'.format(speed, max_length)
-        )
+            f'Змейка | ESC - выход | Скорость: {speed} | '
+        caption += f'Рекорд длины: {max_length}')
         pygame.display.set_caption(caption)
         pygame.display.update()
 
